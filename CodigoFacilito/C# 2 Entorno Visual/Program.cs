9da73a1c721fb4394d69dc15f53ba574ef846067
Elias_Rojas <amzedel7@gmail.com>
2023-11-09 22:41:58 -0400

    video 1

---
+++ CodigoFacilito/C# 2 Entorno Visual/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace presentacion1 {
+    class Program {
+        static void Main(string[]args)
+        {
+            // este es un comentario de Hola Mundo
+            /* este es un comentario 
+            de bloque en varias filas */
+            Console.WriteLine("Hola Mundo");
+            Console.ReadKey();
+ 
+        }
+    }
+}
