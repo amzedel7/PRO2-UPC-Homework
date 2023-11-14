03864911579b5c728e65a126beecc3f5d9648d22
Elias_Rojas <amzedel7@gmail.com>
2023-11-14 13:49:13 -0400

    avnce de del repaso del examen

--- Actividad 8 Algoritmos/ClsEnteros.cs
+++ Actividad 8 Algoritmos/ClsEnteros.cs
@@ -2,13 +2,43 @@ public class ClsEnteros
 {
     public int[]ArrayEnteros;
 
-    public ClsEnteros()
-    {
-        ArrayEnteros = new int[0];
-    }
     public ClsEnteros(int cantidad)
     {
-        ArrayEnteros = new int(cantidad);
+        ArrayEnteros = new int[cantidad];
     }
+    
+
+    public void MostrarEnteros()
+    {
+        string resp = "[";
+        for (int i = 0; i < ArrayEnteros.Length; i++)
+        {
+            resp += ArrayEnteros[i] + ", ";
+        }
+        Console.WriteLine(resp.TrimEnd(',', ' ') + "]");
+    }
+    public void SumarEnterosArray()
+    {
+        int suma = 0;
+        
+        for (int i = 0; i < ArrayEnteros.Length; i++)
+        {
+            suma = suma + ArrayEnteros[i];
+        }
+        Console.WriteLine("La suma es: " + suma);
+    }
+    public void MostrarMayor()
+    {
+        int mayor = ArrayEnteros[0];
+        for (int i = 1; i < ArrayEnteros.Length; i++)
+        {
+            if (ArrayEnteros[i] > mayor)
+            {
+                mayor = ArrayEnteros[i];
+            }
+        }
+        Console.WriteLine("El numero mayor es: " + mayor);
+    }
+    
 }
     
--- Actividad 8 Algoritmos/Program.cs
+++ Actividad 8 Algoritmos/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        ClsEnteros A = new ClsEnteros(5);
+        A.ArrayEnteros[0] = 1;
+        A.ArrayEnteros[1] = 2;
+        A.ArrayEnteros[2] = 3;
+        A.ArrayEnteros[3] = 4;
+        A.ArrayEnteros[4] = 5;
+        A.MostrarEnteros();
+
+        ClsEnteros B = new ClsEnteros(4);
+        B.ArrayEnteros[0] = 6;
+        B.ArrayEnteros[1] = 7;
+        B.ArrayEnteros[2] = 8;
+        B.ArrayEnteros[3] = 9;
+        B.MostrarEnteros();
+        Console.WriteLine("----------------");
+        A.SumarEnterosArray();
+        B.SumarEnterosArray();
+        A.MostrarMayor();
+        B.MostrarMayor();
+        Console.ReadKey();
+        
+
+    
+
+
+         
+    }
+  }
+}
