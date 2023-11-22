396e65bf7e8f4f1be5e67bb5eb7b2b798ae5254a
Elias_Rojas <amzedel7@gmail.com>
2023-11-21 20:27:11 -0400

    mas factoriales

--- Actividad 10 Recursividad/Program.cs
+++ Actividad 10 Recursividad/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("El factorial de 6 es: " + CalculoFactorial.Factorial(6));
+            Console.WriteLine("El factorial de 5 es: " + CalculoFactorial.Factorial(5));
+            Console.WriteLine("El factorial de 4 es: " + CalculoFactorial.Factorial(4));
         }
     }
 }
