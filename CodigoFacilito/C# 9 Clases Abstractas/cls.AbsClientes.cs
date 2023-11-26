4654dc2bdfa6cafeab3b7225d7c992e194e388d3
Elias_Rojas <amzedel7@gmail.com>
2023-11-26 19:03:17 -0400

    video 9

---
+++ CodigoFacilito/C# 9 Clases Abstractas/cls.AbsClientes.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Presentacion7
+{
+  public abstract class clsAbsClientes
+  {
+    public abstract int Id { get; set; }
+    public abstract string Nombre { get; set; }
+    public abstract string Clave { get; set; }
+    public abstract string RFC { get; set; }
+    public abstract int TipoRegimen { get; set; }
+    public abstract string NombreContacto { get; set; }
+  }
+}
